a131f1959178603c25e78ab27dc47ba366888757
Mark Nielsen <marknielsen7@outlook.com>
2017-10-27 15:44:53 -0600

    auth0 is working changed home route

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login/Login'
-import Dashboard from './components/Dashboard/Dashboard';
+import Search from './components/Search/Search';
 import Profile from './components/Profile/Profile';
 import EditProfile from './components/EditProfile/EditProfile';
 
@@ -12,8 +12,8 @@ export default function Router() {
 return ( 
 
 <Switch>
-    <Route path='/auth/login' component={ Login }/>
-    <Route path='/dashboard' component={ Dashboard } />
+    <Route path='/' component={ Login } exact />
+    <Route path='/search' component={ Search } />
     <Route path='/profile' component={ Profile } />
     <Route path='/edit/profile' component={ EditProfile } />
 </Switch>
