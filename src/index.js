0039baf60aec713926b173ccc349aacf270c12db
Mark Nielsen <marknielsen7@outlook.com>
2017-10-27 13:52:00 -0600

    set up auth

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import registerServiceWorker from './registerServiceWorker';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-<HashRouter>
-    
-<App />
+    <HashRouter>
 
-</HashRouter>
+        <App />
 
-, document.getElementById('root'));
+    </HashRouter>
+
+    , document.getElementById('root'));
 registerServiceWorker();
