ab7acaa3f4be5f2207b649ad4c29842142bd62ef
Stefano Martinengo <stefanomartinengo3@gmail.com>
2017-10-27 13:53:59 -0600

    fixed routing

--- server/index.js
+++ server/index.js
@@ -13,42 +13,42 @@ const express = require('express')
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use(session({
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session());
+// app.use(session({
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: true
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session());
 
-massive(process.env.CONNECTION_STRING).then((db)=>{
-    app.set('db',db)
-})
+// massive(process.env.CONNECTION_STRING).then((db)=>{
+//     app.set('db',db)
+// })
 
-passport.use(new Auth0Strategy({
-    domain:process.env.AUTH_DOMAIN,
-    clientID:process.env.AUTH_CLIENTID,
-    clientSecret:process.env.AUTH_CLIENT_SECRET,
-    callbackURL:process.env.AUTH_CALLBACK
-},(accessToken,refreshToken,extraParams,profile,done)=>{
-    const db=app.get('db')
+// passport.use(new Auth0Strategy({
+//     domain:process.env.AUTH_DOMAIN,
+//     clientID:process.env.AUTH_CLIENTID,
+//     clientSecret:process.env.AUTH_CLIENT_SECRET,
+//     callbackURL:process.env.AUTH_CALLBACK
+// },(accessToken,refreshToken,extraParams,profile,done)=>{
+//     const db=app.get('db')
 
-}))
+// }))
 
-passport.serializeUser((profile,done)=>{
-    done(null,profile)
-})
-passport.deserializeUser((profile,done)=>{
-    done(null,profile)
-})
+// passport.serializeUser((profile,done)=>{
+//     done(null,profile)
+// })
+// passport.deserializeUser((profile,done)=>{
+//     done(null,profile)
+// })
 
 //endpoints//
 
-app.get('/auth/login',passport.authenticate('auth0'))
+// app.get('/auth/login',passport.authenticate('auth0'))
 
-app.get('/auth/callback',passport.authenticate('auth0',{
-    sucessRedirect:'https://localhost:3000/dashboard',
-    failureRedirect:'/auth/login'
-}))
+// app.get('/auth/callback',passport.authenticate('auth0',{
+//     sucessRedirect:'https://localhost:3000/dashboard',
+//     failureRedirect:'/auth/login'
+// }))
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
