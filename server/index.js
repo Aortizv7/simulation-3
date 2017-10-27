0ca350be9f971ca4c3d52af5b7079138ed15e7a6
Ana Ortiz <ana.ortiz.y.b.7@gmail.com>
2017-10-27 11:30:26 -0600

    downloaded dependencies and set up basic server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express =require('express')
+    ,bodyParser=require('body-parser')
+    ,cors=require('cors')
+    ,massive=require('massive')
+    ,app=express()
+    ,PORT=8080
+
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+
+
+
+
+app.listen(PORT,()=>console.log(`Listening on port: ${PORT}`));
